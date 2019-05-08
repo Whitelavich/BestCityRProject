a41c25a07be86c394bc548bad3b3ad2278c16632
Whitelavich <arw21@pct.edu>
2019-05-08 00:04:33 -0400

    produced output

--- BestCityRProject/Script1.R
+++ BestCityRProject/Script1.R
@@ -5,6 +5,7 @@ library(maps)
 library(plyr)
 library(dplyr)
 library(ggplot2)
+library(htmltab)
 
 cities <- as.data.frame(us.cities)
 cities <- subset(cities, select = c("name", "lat", "long"))
@@ -58,7 +59,18 @@ for (index in 1:nrow(museum)) {
 museumCount <- count(museum, City)
 colnames(museumCount) <- c("City", "Number_Of_Museums")
 final <- merge(final, museumCount, by = "City")
+for (index in 1:nrow(final)) {
+    final[index, "CustomScore"] <- as.numeric(final[index, "NumHospitals"]) * as.numeric(final[index, "Median_Income"]) / as.numeric(final[index, "crime_rate"]) * as.numeric(final[index, "Number_Of_Museums"])
+}
 
+happinessTbl <- htmltab::htmltab("https://wallethub.com/edu/happiest-places-to-live/32619/")
+
+colnames(happinessTbl) <- c("Rank", "City", "Score", "wellbeing_rank", "income_rank", "community_rank")
+happinessTbl <- subset(happinessTbl, select = c("Rank", "City", "Score"))
+
+for (index in 1:nrow(happinessTbl)) {
+    happinessTbl[index, "City"] <- tolower(happinessTbl[index, "City"])
+}
 
 CountGraph <- ggplot(data = final, aes(x = reorder(City,-NumHospitals))) +
      theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
@@ -81,3 +93,26 @@ crimeGraph
 
 #SO heres my thought if we sort by the different column and then average the indexes of the cities we can produce an overall score
 #for each city
+
+
+worldMap <- map_data("usa")
+?map_data
+base_world <- ggplot() + coord_fixed() +
+    xlab("") + ylab("") + geom_polygon(data = worldMap, aes(x = long, y = lat, group = group),
+                               colour = "black", fill = "light green")
+base_world
+
+
+colors<-rainbow(nrow(final))
+map_data <-
+    base_world +
+    geom_point(data = final, aes(x = long, y = lat,size=CustomScore),color=colors)
+map_data
+
+scoreChart <- ggplot(data = final, aes(x = reorder(City, - CustomScore), y = CustomScore, group = 1), color = "hot pink") + geom_line(color = "hot pink") +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1))
+scoreChart
+
+
+happiestCities <- head(happinessTbl, 20)
+saddestCities<-tail(happinessTbl,20)
